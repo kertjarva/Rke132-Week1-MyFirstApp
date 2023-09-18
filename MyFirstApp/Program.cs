92a4080c27e39a6b9532dd4745d973a9640731a2
Kert Järva <kert.jarva@tktk.ee>
2023-09-18 10:20:37 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string- sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello, " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
